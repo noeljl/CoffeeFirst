e9c415529977a6cb59d93cb783b939d61bf181ba
= <>
2024-08-22 12:01:38 +0200

    Anpassung Cors IV

--- backend/loaders/express.js
+++ backend/loaders/express.js
@@ -26,17 +26,20 @@ const expressLoader = (app) => {
           callback(new Error('Not allowed by CORS'))
         }
       },
-      methods: 'GET,POST,PUT,DELETE',
-      allowedHeaders: 'Content-Type,Authorization',
+      methods: 'GET, POST, PUT, DELETE, OPTIONS, PATCH', // Hinzufügen der Methoden, die von der API unterstützt werden
+      allowedHeaders: 'Content-Type, Authorization', // Erlaubt den Zugriff auf diese Header
       credentials: true, // Erlaubt Cookies und andere Anmeldeinformationen, wenn nötig
     })
   )
 
-  // Transformiert einen String zu einem JSON String, damit weiterverarbietet werden kann
+  // Handle OPTIONS preflight requests
+  app.options('*', cors()) // Dies stellt sicher, dass alle OPTIONS-Anfragen mit den entsprechenden CORS-Headern beantwortet werden
+
+  // Transformiert einen String zu einem JSON String, damit weiterverarbeitet werden kann
   app.use(bodyParser.json())
 
-  // Die in den Anfragen übergebenen String werden geparst
-  // Gibt jemand in einer Anfrage einen /?name=Laurens, dann wird der Paramter in
+  // Die in den Anfragen übergebenen Strings werden geparst.
+  // Gibt jemand in einer Anfrage einen /?name=Laurens an, dann wird der Parameter in
   // {
   // name: Laurens
   // } geparst
@@ -45,14 +48,14 @@ const expressLoader = (app) => {
   // Um Express-Server mitzuteilen, dass er sich hinter einem vertrauenswürdigen Reverse-Proxy (wie z.B. Nginx, HAProxy oder einem Load Balancer)
   // befindet. Dies hat Auswirkungen darauf, wie die Anwendung bestimmte Informationen, die durch den Proxy weitergeleitet werden, verarbeitet,
   // insbesondere die IP-Adresse des Clients.
-  // Der Reverse Proxy leitet dann ja Anfragen an den Clienten in Form von HTML Daten weiter
+  // Der Reverse Proxy leitet dann Anfragen an den Client in Form von HTML-Daten weiter
   // und kann Anfragen entgegennehmen. Daher muss diesem vertraut werden
   app.set('trust proxy', 1)
 
   // Erstellt eine Session für State Management
   app.use(
     session({
-      // Zum identifizieren des Nutzers. Dies verhindert, dass jemand die Sitzung manipuliert.
+      // Zum Identifizieren des Nutzers. Dies verhindert, dass jemand die Sitzung manipuliert.
       secret: SESSION_SECRET,
       // Wenn resave auf false gesetzt ist, wird die Sitzung nur dann erneut gespeichert, wenn sie tatsächlich verändert wurde. Dies verhindert unnötige Speicheroperationen und verbessert die Effizienz.
       resave: false,
@@ -60,7 +63,7 @@ const expressLoader = (app) => {
       saveUninitialized: false,
       cookie: {
         // Die secure-Option legt fest, ob das Cookie nur über HTTPS gesendet werden soll. Wenn secure: false gesetzt ist, wird das Cookie auch über HTTP gesendet.
-        secure: true, //Sobald ssl 0> true
+        secure: true, // Sobald SSL aktiviert ist, sollte secure auf true gesetzt werden
         // maxAge gibt die Lebensdauer des Sitzungscookies in Millisekunden an. In diesem Fall ist das Cookie 24 Stunden gültig (24 Stunden * 60 Minuten * 60 Sekunden * 1000 Millisekunden).
         maxAge: 24 * 60 * 60 * 1000,
       },
