e3bbcf26427020d4d49bb9b1a21db6fea639793b
= <>
2024-08-23 08:36:48 +0200

    Anpassung Cors V

--- frontend/src/apis/client.js
+++ frontend/src/apis/client.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 
 const API = axios.create({
   // baseURL: 'http://localhost:3001/api', // Für lokale Entwicklung
-  // baseURL: 'http://backend:3001/api',
-  baseURL: 'https://www.rotaract-district-1866.de/api',
+  baseURL: 'http://backend:3001/api',
+  // baseURL: 'https://www.rotaract-district-1866.de/api',
   headers: {
     'Content-Type': 'application/json',
   },
