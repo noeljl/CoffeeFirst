7a14408e641805b4e7548f4e71deef4af79f6944
Jan Laurens Ohl <janlaurens.ohl@gmail.com>
2025-07-04 13:22:19 +0200

    Add SignIn.jsx page

--- frontend/src/pages/signin/SignIn.jsx
+++ frontend/src/pages/signin/SignIn.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
-import NavBar from '../../components/ui/navbar/Navbar'
+import NavBar from '../../components/ui/navbar/Navbar.jsx'
 import Footer from '../../components/footer/Footer'
 import SignInForm from '../../components/signin-form/SignInForm.jsx'
-import '../../components/signin-form/SignInForm.css'
+// import "./SignUp.css";
 
 function SignIn() {
   return (
-    <div className="signup-form-section">
+    <div>
       <NavBar />
-      <div className="signup-form-text">
-        <h2 style={{ textAlign: 'center', fontSize: '32px', margin: 0 }}>
-          Login
-        </h2>
-        {/* <SignInForm /> */}
+      <div className="signup-form-section">
+        <div className="signup-form-text">
+          <h2
+            style={{
+              textAlign: 'center',
+              fontSize: '2.5rem',
+              marginBottom: '1.5rem',
+            }}
+          >
+            Login
+          </h2>
+          <div className="signupComp">
+            <SignInForm />
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
