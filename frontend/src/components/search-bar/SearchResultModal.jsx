c41513e287cbb9c0ae390c227a3eac4ee3400554
Noel Linder <noel_linder@me.com>
2025-07-17 16:45:44 +0200

    search results now contain a get direction button

--- frontend/src/components/search-bar/SearchResultModal.jsx
+++ frontend/src/components/search-bar/SearchResultModal.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Icons from '../../assets/Icons.js';
 import './SearchBar.css';
+import handleGetDirection from '../../functions/handleGetDirection';
+import { FaDirections } from 'react-icons/fa';
 
 function SearchResultModal({ searchQuery, districts, cafes, onDistrictSelect, onCafeSelect }) {
     const navigate = useNavigate();
@@ -36,7 +38,7 @@ function SearchResultModal({ searchQuery, districts, cafes, onDistrictSelect, on
                 {/* Always show filtered districts */}
                 {filteredDistricts.map((d, idx) => (
                     <li
-                        className="search-result-item"
+                        className="search-result-default"
                         key={d.districtName}
                         onClick={() => onDistrictSelect && onDistrictSelect(d.districtName)}
                         style={{ cursor: 'pointer' }}
@@ -58,14 +60,19 @@ function SearchResultModal({ searchQuery, districts, cafes, onDistrictSelect, on
                         onClick={() => handleCafeClick(c.slug)}
                         style={{ cursor: 'pointer' }}
                     >
+                        <div className="search-result-col-left">
                         <img
-                            src={Icons.coffeeShop}
+                            src={c.images[0]}
                             className="search-result-icon"
                             alt="Cafe"
                         />
                         <div className="search-result-text">
                             <div className="search-result-name">{c.name}</div>
                             <div className="search-result-desc">{c.district}</div>
+                        </div>
+                        </div>
+                        <div className="search-result-col-right">
+                            <FaDirections style={{ height: '30px', width: '30px' }} onClick={() => handleGetDirection(c.coords)} />
                         </div>
                     </li>
                 ))}
