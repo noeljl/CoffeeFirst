1e5f1b121efdf671e9d450cea40a4d51827143b1
Noel Linder <noel_linder@me.com>
2025-07-18 18:50:40 +0200

    RegForm css updated

--- frontend/src/pages/signup/RegForm.jsx
+++ frontend/src/pages/signup/RegForm.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import NavBar from '../../components/navbar/Navbar.jsx'
 import Footer from '../../components/footer/Footer'
 import SignupForm from '../../components/signup-form/SignupForm'
-// import "./SignUp.css";
 
 function RegForm() {
   return (
@@ -14,8 +13,8 @@ function RegForm() {
             STEP <span style={{ fontWeight: 'bold' }}>1</span> OF{' '}
             <span style={{ fontWeight: 'bold' }}>3</span>
           </p>
-          <h2>Enter yor mail and create a password to start your membership</h2>
-          <p className="subtitle">
+          <h2 style={{ fontSize: 'clamp(1.5rem, 3vw, 2rem)', lineHeight: '1.2' }}>Enter yor mail and create a password to start your membership</h2>
+          <p style={{ fontSize: 'clamp(1rem, 2vw, 1.2rem)', margin: '10px 0px 30px 0px' }}>
             Just a few more steps to get your first coffee!
           </p>
           <div className="signupComp">
