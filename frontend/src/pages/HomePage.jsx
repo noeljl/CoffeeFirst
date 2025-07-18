0f14ed22b0e8bdd38ae54182d09b8e87b055783a
Noel Linder <noel_linder@me.com>
2025-07-18 17:47:01 +0200

    CTA css updated

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -15,23 +15,21 @@ function HomePage() {
     <div>
       <NavBar minimal />
       <Hero />
-      <div className="page-frame">
-        <div className="section">
-          <h2 id="section-title">Some of our partners</h2>
+      <div className="homepage-body">
+        <div className="homepage-section">
+          <h2 className="homepage-section-title">Some of our partners</h2>
           <CoffeeGallery />
         </div>
       </div>
-      <div className="section-sustainability">
-        <div className="left-col">
-          <img
-            src="/images/home-sustainability.png"
-            draggable={false}
-            alt="Sustainability"
-          />
-        </div>
-        <div className="right-col">
-          <h2 id="section-title">Coffee meets sustainability</h2>
-          <p style={{ fontSize: 22, paddingTop: 20, paddingRight: 200 }}>
+      <div className="homepage-section sustainability-section">
+        <img
+          src="/images/home-sustainability.png"
+          draggable={false}
+          alt="Sustainability"
+        />
+        <div className="sustainability-section-text">
+          <h2>Coffee meets sustainability</h2>
+          <p>
             At CoffeeFirst, we believe every cup can make a difference. We
             partner exclusively with cafés that prioritize ethically sourced
             beans, eco-friendly packaging, and sustainable production. Our
@@ -40,21 +38,19 @@ function HomePage() {
           </p>
         </div>
       </div>
-      <div className="page-frame">
-        <div className="section">
-          <h2 id="section-title">Pricing</h2>
-          <PricingTable onSelectPlan={null} onSessionCreated={null} page="home" />
-        </div>
+      <div className="homepage-section">
+        <h2 className="homepage-section-title">Pricing</h2>
+        <PricingTable onSelectPlan={null} onSessionCreated={null} page="home" />
       </div>
-      <CTA />
-      <div className="page-frame">
-        <div className="section">
-          <h2 id="section-title">Questions? Answers.</h2>
-          <FAQAccordion faqs={faqs} />
-        </div>
+      <div className="homepage-section">
+        <CTA />
+      </div>
+      <div className="homepage-section">
+        <h2 className="homepage-section-title">Questions? Answers.</h2>
+        <FAQAccordion faqs={faqs} />
       </div>
 
-      
+
       <Footer />
     </div>
   )
