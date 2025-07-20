bdb2eccc16b73677307e80c174fce92830e7f20e
Jan Laurens Ohl <janlaurens.ohl@gmail.com>
2025-07-20 14:13:23 +0200

    Small Fixe

--- frontend/src/layout/ColumnLayout.jsx
+++ frontend/src/layout/ColumnLayout.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { useMediaQuery } from 'react-responsive';
-import Footer from "../components/footer/Footer";
-import NavBar from "../components/navbar/Navbar";
-import MobileMenu from "../components/navbar/MobileMenu";
-import styles from "./styles/ColumnLayout.module.css";
+import React from 'react'
+import { useMediaQuery } from 'react-responsive'
+import Footer from '../components/footer/Footer'
+import NavBar from '../components/navbar/Navbar'
+import MobileMenu from '../components/navbar/mobileMenu'
+import styles from './styles/ColumnLayout.module.css'
 
 // This component provides a two-column layout with a sidebar and main content area. It's just a shell that can be used to design the two column dashboard and settings pages.
 
 function ColumnLayout({ SidebarSlot, children, sidebarType }) {
-  const isTablet = useMediaQuery({ maxWidth: 900 });
+  const isTablet = useMediaQuery({ maxWidth: 900 })
 
   return (
     <>
@@ -18,14 +18,12 @@ function ColumnLayout({ SidebarSlot, children, sidebarType }) {
           <SidebarSlot type={sidebarType} />
         </aside>
 
-        <main className={styles.main}>
-          {children}  
-        </main>
+        <main className={styles.main}>{children}</main>
       </div>
       <Footer />
       {isTablet && <MobileMenu />}
     </>
-  );
+  )
 }
 
-export default ColumnLayout;
+export default ColumnLayout
